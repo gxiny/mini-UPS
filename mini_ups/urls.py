8a7f3766d84f73fc9da390bcceda6c4facc0064f
xg41 <xg41@duke.edu>
2018-04-25 19:49:12 -0400

    remove wrong page

--- mini_ups/urls.py
+++ mini_ups/urls.py
@@ -6,11 +6,8 @@ urlpatterns = [
     url(r'^login/$', views.signin,name='login'),
     url(r'^regist/$', views.regist,name='regist'),
     url(r'^logout/$',views.signout,name='logout'),
-    url(r'^wrong/$',views.wrong,name='wrong'),
     url(r'^home/$', views.homepage, name='homepage'),
     url(r'^search/$', views.searchpage, name='searchpage'),
-    #url(r'^search_res/$',views.search_res, name='search_res'),
-    #url(r'^redirect/$',views.Redirectpage, name='Redirectpage'),
     url(r'^ups/$',views.ups, name='ups'),
     url(r'^redirect/(?P<package_id>\d+)$',views.Redirectpage, name='Redirectpage'),
 ]
