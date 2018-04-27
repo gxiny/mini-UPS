c5dad222956d2f8c45f2ae64304de26e77af194a
rz78 <rz78@duke.edu>
2018-04-27 14:32:26 -0400

    start.sh: wait for postgres to be up

--- frontend/start.sh
+++ frontend/start.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
+while true; do
+	nc -z "${POSTGRES_HOST:-localhost}" "${POSTGRES_PORT:-5432}"
+	if [ "$?" = "0" ]; then
+		break
+	fi
+	echo "Waiting for postgres..."
+	sleep 1
+done
 ./manage.py migrate && \
 ./manage.py collectstatic --no-input && \
 exec ./start.py
